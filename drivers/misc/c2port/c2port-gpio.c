bfbc59f4af16712955500e100942fc4746263dae
Florin Uifelean <florin_uifelean@yahoo.com>
2015-05-03 18:56:02 +0100

    c2 gpio registers used

--- drivers/misc/c2port/c2port-gpio.c
+++ drivers/misc/c2port/c2port-gpio.c
@@ -27,6 +27,65 @@ MODULE_PARM_DESC(c2ck, "C2CK pin");
 module_param(c2d, int, S_IRUSR);
 MODULE_PARM_DESC(c2d, "C2D pin");
 
+int  mem_fd;
+void *gpio_map;
+
+// I/O access
+volatile unsigned *gpio;
+
+
+// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+
+#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
+
+#define GET_GPIO(g) (*(gpio+13)&(1<<g)) // 0 if LOW, (1<<g) if HIGH
+
+#define GPIO_PULL *(gpio+37) // Pull up/pull down
+
+#define GPIO_C2D_PORT           23
+#define GPIO_C2CKSTB_PORT       24
+#define GPIO_C2CK_PORT          24
+
+//
+// Set up a memory regions to access GPIO
+//
+void setup_io()
+{
+	/* open /dev/mem */
+	if ((mem_fd = open("/dev/mem", O_RDWR | O_SYNC)) < 0) {
+		printf("can't open /dev/mem \n");
+		exit(-1);
+	}
+
+	/* mmap GPIO */
+	gpio_map = mmap(
+		NULL,             //Any adddress in our space will do
+		BLOCK_SIZE,       //Map length
+		PROT_READ | PROT_WRITE,// Enable reading & writting to mapped memory
+		MAP_SHARED,       //Shared with other processes
+		mem_fd,           //File to map
+		GPIO_BASE         //Offset to GPIO peripheral
+		);
+
+	close(mem_fd); //No need to keep mem_fd open after mmap
+
+	if (gpio_map == MAP_FAILED) {
+		printf("mmap error %d\n", (int)gpio_map);//errno also set!
+		exit(-1);
+	}
+
+	// Always use volatile pointer!
+	gpio = (volatile unsigned *)gpio_map;
+
+
+} // setup_io
+
+
+
 /* --- C2 port operations --------------------------------------------------- */
 
 static void gpio_c2port_access(struct c2port_device *dev, int status)
@@ -52,17 +111,40 @@ static void gpio_c2port_c2d_dir(struct c2port_device *dev, int dir)
 
 static int gpio_c2port_c2d_get(struct c2port_device *dev)
 {
-	return gpio_get_value(c2d);
+//	return gpio_get_value(c2d);
+	int gpioval;
+	gpioval = GET_GPIO(GPIO_C2D_PORT);
+
+	//printf("%d\n", gpioval);
+
+	return gpioval != 0;
 }
 
 static void gpio_c2port_c2d_set(struct c2port_device *dev, int status)
 {
-	gpio_set_value(c2d, status);
+//	gpio_set_value(c2d, status);
+	if (status)
+	{
+		GPIO_SET = 1 << GPIO_C2D_PORT;
+		INP_GPIO(GPIO_C2D_PORT);
+		OUT_GPIO(GPIO_C2D_PORT);
+	}
+	else
+		GPIO_CLR = 1 << GPIO_C2D_PORT;
 }
 
 static void gpio_c2port_c2ck_set(struct c2port_device *dev, int status)
 {
-	gpio_set_value(c2ck, status);
+//	gpio_set_value(c2ck, status);
+	if (status)
+	{
+		INP_GPIO(GPIO_C2CK_PORT);
+		OUT_GPIO(GPIO_C2CK_PORT);
+
+		GPIO_SET = 1 << GPIO_C2CK_PORT;
+	}
+	else
+		GPIO_CLR = 1 << GPIO_C2CK_PORT;
 }
 
 static struct c2port_ops gpio_c2port_ops = {
@@ -96,6 +178,8 @@ static int __init gpio_c2port_init(void)
 	gpio_c2port_dev = c2port_device_register("uc", &gpio_c2port_ops, NULL);
 	if (!gpio_c2port_dev)
 		return -ENODEV;
+
+	setup_io();
 
 	return 0;
 
